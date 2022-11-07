18aefbf52150fe3eb8106d3339ef67eb9ddfdf55
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2022-11-07 15:50:02 +0530

    Bean Lifecycle using Interface

---
+++ src/main/java/com/springcore/beanLifecycle/Pepsi.java
@@ -0,0 +1,40 @@
+package com.springcore.beanLifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Pepsi implements InitializingBean, DisposableBean {
+    private double price;
+
+    public Pepsi() {
+    }
+
+    public Pepsi(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Pepsi{" +
+                "price=" + price +
+                '}';
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Bean lifecycle using interface - destroyed");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Bean lifecycle using interface - created");
+    }
+}
